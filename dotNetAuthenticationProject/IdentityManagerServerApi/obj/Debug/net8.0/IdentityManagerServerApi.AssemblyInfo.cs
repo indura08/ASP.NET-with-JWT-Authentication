74c093952453a0840657e51b85f81bec4272b86f
indura08 <induraperera3@gmail.com>
2024-07-31 17:57:28 +0530

    new commit

--- dotNetAuthenticationProject/IdentityManagerServerApi/obj/Debug/net8.0/IdentityManagerServerApi.AssemblyInfo.cs
+++ dotNetAuthenticationProject/IdentityManagerServerApi/obj/Debug/net8.0/IdentityManagerServerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityManagerServerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a381916052fde60bf89576c087222283f5be66bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c9c4b347aa4342edef5fa6f32e10a5bc571675")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityManagerServerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityManagerServerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
